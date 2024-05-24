ad9347e3ff999ab7535389065da3686c77da969a
Alex Nascimento <alexjose68@hotmail.com>
2024-05-24 18:06:51 -0300

    feat: Structuring and styling the base of the App and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import './App.css'
 
-function App() {
+import logoImg from './assets/logo.png';
 
+function App() {
   return (
     <>
-      <div>
-        <h1>TESTE</h1>
+      <div className='container'>
+        <img src={logoImg} alt="Logo da calculadora de gasolina e etanol" />
+        <h1 className='title'>Qual melhor opção?</h1>
+
+        <form className='form'>
+          <label htmlFor="ethanol">Álcool (preco por litro):</label>
+          <input
+            id='ethanol'
+            className='input'
+            type='number'
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label htmlFor="gasoline">Álcool (preco por litro):</label>
+          <input
+            id='gasoline'
+            className='input'
+            type='number'
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input type="submit" className='button' value="Calcular" />
+        </form>
       </div>
       
     </>
